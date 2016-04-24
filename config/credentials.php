bdfcc7d70ce331e5a0b33802dc6bd46280eafcca
Pierre Grimaud <pierre@shopping-flux.com>
2016-04-24 17:44:19 +0200

    composer + credentials

---
+++ config/credentials.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'config' => [
+        'client_id' => 'TP0QDOTJRXFIQUL44N2MFA1DITASS4QGZBZDA00POPKJGB1C',
+        'client_secret' => 'XL43SXCWMMPZMNVTZBCSEW0SHBYTXFKL5HUSSHDSMNPWRBXH',
+        'redirect_uri' => 'http://p.ier.re/swarm-sdk/redirect'
+    ],
+    'entrypoints' => [
+        'auth' => 'https://foursquare.com/oauth2/authenticate?client_id=Y%client_id%&response_type=code&redirect_uri=%redirect_uri%',
+        ''
+    ]
+];
